15cf77ec59bf9f64961940c80c8e7c9fd4072a73
Radomianka83 <kamilaciezka@gmail.com>
2024-11-01 07:25:44 +0000

    Add files via upload

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+<DOCTYPE! html>
+<html>
+<script>
+function My_First_Function() {var String = "Like me,I'm Polish!"; var result = String.fontcolor("red"); document.getElementById("Polish").innerHTML = RESULT;}
+</script>
+</html>
